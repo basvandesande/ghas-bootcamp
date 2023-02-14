0d8206e0750079e612e07dc66b002502bd079dcf
Bas van de Sande <bvandesande@xpirit.com>
2023-02-14 17:30:12 +0100

    added pre setup project to repo

---
+++ CodeScanningSolutiuon/MyConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MyConsoleApp
+{
+    public class Program
+    {
+        // force warning in codeQL
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+            Console.Write("This is a test");
+
+            if (true)
+            {
+
+            }
+            else
+            {
+                // do nothing    
+
+            }
+
+            int a =1;
+            if (a>2)
+            {
+
+            }
+            else
+            {
+               Console.WriteLine("some random code inserted by me!");     
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
